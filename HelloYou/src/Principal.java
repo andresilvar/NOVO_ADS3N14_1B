c95bf03d5a18814c46be5517f43b3d3526953e24
andre <andre_silvar@yahoo.com.br>
2014-04-29 23:34:14 -0300

    HelloWolrd foi modificado para HelloYou
    HelloWolrd foi modificado para HelloYou

---
+++ HelloYou/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, You");
+		// TODO Auto-generated method stub
+
+	}
+
+}
